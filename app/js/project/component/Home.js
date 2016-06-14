6f2e280d1758d3f13f99e845f7276cafc3c37bcb
onehunnid <climbrick1989@gmail.com>
2016-06-14 11:16:06 -0400

    turned practice into sign up/sign in flow

--- app/js/project/component/Home.js
+++ app/js/project/component/Home.js
@@ -1,24 +1,57 @@
-// NOT BEING USED AT THE MOMENT
-// import React, { Component } from 'react';
-//
-// // This is a Plain Javascript Class using ES6 syntax
-// class Home extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-//   render() {
-//     console.log(this.state);
-//
-//     return (
-//       <div>
-//         <h1>Welcome to Reactland!</h1>
-//         <p>This area is where you can practice react.</p>
-//         <button>Get Started</button>
-//       </div>
-//     );
-//   }
-// }
-//
-// // Export Home Class
-// export default Home;
+import React from 'react';
+
+class Intro extends React.Component {
+  constructor(props) {
+    super(props); // super() passes props to the component
+    this.handlePrevious = this.handlePrevious.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.state = {next: false};
+  }
+
+  handleLogin() {
+    this.setState({next: true});
+  }
+
+  handlePrevious() {
+    this.setState({next: false});
+  }
+
+  render() {
+    console.log("Current State ", this.state)
+
+    if (this.state.next === false) {
+      return (
+        <div className="intro">
+          <h1 className="logo">Listmark</h1>
+          <div className="tagline">The Superior Bookmark Manager.</div>
+          <p className="intro-desc">Listmark is the easiest and quickest way to save and share links you find on the web. </p>
+          <div className="button" onClick={this.handleLogin}><a href="#">SIGN UP / SIGN IN</a></div>
+          <div className="button-learn-more"><a href="#">LEARN MORE</a></div>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="intro">
+          <h1 className="logo">Listmark</h1>
+          <div className="tagline">The Superior Bookmark Manager.</div>
+          <p className="intro-desc">Listmark is the easiest and quickest way to save and share links you find on the web. </p>
+          <div className="button-wrapper">
+            <div className="button-social button--twitter" data-login="google">
+              Continue with Twitter
+            </div>
+            <div className="button-social button--facebook" data-login="google">
+              Continue with Facebook
+            </div>
+            <div className="button-social button--google" data-login="google">
+              Continue with Google
+            </div>
+          </div>
+          <div className="button-cancel" onClick={this.handlePrevious}>Cancel</div>
+        </div>
+      );
+    }
+  }
+}
+
+export default Intro;
--- app/js/project/index.js
+++ app/js/project/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+import Intro from './component/Home.js';
+
 // This class displays the Intro class in the DOM
 class DisplayOnScreen {
   constructor(props) {
@@ -11,49 +13,6 @@ class DisplayOnScreen {
       <Intro />,
       document.querySelector('.react-container')
     );
-  }
-}
-
-// This is a Plain Javascript Class using ES6 syntax
-// This is what controls the Intro message
-class Intro extends React.Component {
-  // The constructor to get state replaces getInitialState
-  constructor(props) {
-    super(props); // super() passes props to the component
-    this.handlePrevious = this.handlePrevious.bind(this); //Binds this
-    this.handleNext = this.handleNext.bind(this);// Binds this
-    this.state = {next: false}; // Sets the default state to false
-  }
-  // Sets the state to true
-  handleNext() {
-    this.setState({next: true});
-  }
-  // Sets the state to false
-  handlePrevious() {
-    this.setState({next: false});
-  }
-  // renders the content
-  render() {
-    console.log("Current State ", this.state)
-
-    if (this.state.next === true) {
-      return (
-        <div className="intro">
-          <h1>Welcome to Reactland!</h1>
-          <p>You clicked a button. Great job!</p>
-          <button onClick={this.handlePrevious}>cancel</button>
-        </div>
-      );
-    }
-    else {
-      return (
-        <div className="intro">
-          <h1>Welcome to Reactland!</h1>
-          <p>This area is where you can practice react. Click the button to continue.</p>
-          <button onClick={this.handleNext}>Get Started</button>
-        </div>
-      );
-    }
   }
 }
 
